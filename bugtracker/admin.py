d934a002afa0663dce31df73941ec912908ebe5c
Olexander Yermakov <mannavard1611@gmail.com>
2014-10-02 15:06:10 +0300

    Admin improvements

--- bugtracker/admin.py
+++ bugtracker/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Ticket
 
-# Register your models here.
-admin.site.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('closed', 'title', 'text', 'created', 'user')
+    list_filter = ['created', 'closed']
+    search_fields = ['title', 'text']
+
+
+admin.site.register(Ticket, TicketAdmin)
