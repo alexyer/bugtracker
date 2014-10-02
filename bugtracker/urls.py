c9d6f6873989526ac3c5c72c06084ed4260da174
Olexander Yermakov <mannavard1611@gmail.com>
2014-10-02 15:00:07 +0300

    Init

---
+++ bugtracker/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8; -*-
+#!/usr/bin/env python
+
+__author__ = 'Olexander Yermakov'
+__email__ = 'mannavard1611@gmail.com'
+
+
+from django.conf.urls import patterns, include, url
+
+
+urlpatterns = patterns('',
+)
