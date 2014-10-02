8ffe1a71c85215c21acd3124dc37c4bca6f4f9c7
Olexander Yermakov <mannavard1611@gmail.com>
2014-10-02 15:50:21 +0300

    Register

--- bugtracker/urls.py
+++ bugtracker/urls.py
@@ -9,9 +9,11 @@ from django.conf.urls import patterns, include, url
 
 from .views import BugListView
 from .views import BugDetailView
+from .views import RegisterView
 
 
 urlpatterns = patterns('',
         url(r'^$', BugListView.as_view(), name='index'),
         url(r'^(?P<pk>[0-9]+)/$', BugDetailView.as_view(), name='detail'),
+        url(r'^register/$', RegisterView.as_view(), name='register'),
 )
--- bugtracker/views.py
+++ bugtracker/views.py
@@ -1,4 +1,10 @@
+from django.contrib.auth.forms import UserCreationForm
+
+from django.core.urlresolvers import reverse_lazy
+
 from django.shortcuts import render
+
+from django.views.generic import CreateView
 from django.views.generic import ListView
 from django.views.generic import DetailView
 
@@ -7,9 +13,15 @@ from .models import Ticket
 
 class BugListView(ListView):
     model = Ticket
-    template_name = "list.html"
+    template_name = 'list.html'
 
 
 class BugDetailView(DetailView):
     model = Ticket
-    template_name = "detail.html"
+    template_name = 'detail.html'
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('index')
